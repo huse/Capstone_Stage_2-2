2fe3837445f9dfdf91fe1a3e3417fa4867c98025
husen <hus.kpr@gmail.com>
2018-05-01 15:47:46 -0700

    Adding recyclerview ti main activity

--- app/src/main/java/com/hpr/hus/capstone_stage_2/MainActivity.java
+++ app/src/main/java/com/hpr/hus/capstone_stage_2/MainActivity.java
@@ -2,12 +2,58 @@ package com.hpr.hus.capstone_stage_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener{
+    RecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // populate  RecyclerView
+        ArrayList<String> placeHolder = new ArrayList<>();
+        placeHolder.add("Message 1");
+        placeHolder.add("Message 2");
+        placeHolder.add("Message 3");
+        placeHolder.add("Message 4");
+        placeHolder.add("Message 5");
+
+        // setting up RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvMessages);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(this, placeHolder);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        // setup layoutManager
+
+        LinearLayoutManager  layoutManager = new LinearLayoutManager (this) {
+            @Override
+            public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+                return null;
+            }
+        };
+
+        //Adding divider
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+
+
     }
+    //Adding action lestenter to recyclereview.
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ app/src/main/java/com/hpr/hus/capstone_stage_2/RecyclerViewAdapter.java
@@ -0,0 +1,75 @@
+package com.hpr.hus.capstone_stage_2;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data passed to constructor
+    RecyclerViewAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates row layout from xml whenever needs
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // bindING   data to TextView in row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+    }
+
+    //number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // recycles views , stores as they will be scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.tvMessageName);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // method for getting data at clicked position
+    String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allowing to click event to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity would implement thhis methods for responding to click event
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
